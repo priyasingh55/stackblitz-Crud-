7c691eb92847b1254c955bd5c0a2efaa7d395405
priyasingh55 <priyasingh55664@gmail.com>
2025-05-23 22:46:37 +0530

    multiple item delete

--- CrudBasics.js
+++ CrudBasics.js
@@ -4,13 +4,15 @@ let contentInput = document.getElementById('contentInput');
 let msg = document.getElementById('msg');
 let posts = document.getElementById('posts');
 let headingInput = document.getElementById('headingInput');
+let deleteSelectedBtn = document.getElementById('deleteSelectedBtn');
+
 
 form.addEventListener('submit', (e) => {
-  //to prevent from refreshing page every time when we submit use this e.preventDefault()
   e.preventDefault();
   console.log('button clicked');
   formValidation();
 });
+
 
 let formValidation = () => {
   if (contentInput.value === '' || headingInput.value === '') {
@@ -23,51 +25,57 @@ let formValidation = () => {
   }
 };
 
-//accept and store data
 let data = {};
 
-//read operation
 let acceptData = () => {
   data = { heading: headingInput.value, content: contentInput.value };
   console.log('Data pushed..', data);
   createPost();
 };
-//upload data on screen //CREATE operation
+
+
 let createPost = () => {
   posts.innerHTML += `
   <div>
-  <p>
-  <strong>${data.heading}</strong>
-  <br />
-  <span>${data.content}</span>
-</p>
-  <span class="options">
-    <i onClick="editPost(this)" class="fa-solid fa-pen-to-square"></i>
-    
-    <i  onClick="deletePost(this)" class="fa-solid fa-trash"></i>
-  </span>
-</div> 
+    <input type="checkbox" class="post-checkbox">
+    <p>
+      <strong>${data.heading}</strong>
+      <br />
+      <span>${data.content}</span>
+    </p>
+    <span class="options">
+      <i onclick="editPost(this)" class="fa-solid fa-pen-to-square"></i>
+      <i onclick="postdelete(this)" class="fa-solid fa-trash"></i>
+    </span>
+  </div> 
   `;
-  //after writting clear the input
   headingInput.value = '';
   contentInput.value = '';
 };
 
-//delete operation
-//window. is used to make function global
-window.deletePost = (e) => {
-  //delete entire things
-  e.parentElement.parentElement.remove();
-};
-
-//update operation
 
 window.editPost = (e) => {
   const postElement = e.parentElement.parentElement;
   const heading = postElement.querySelector('strong').textContent;
   const content = postElement.querySelector('span').textContent;
-
+  
   headingInput.value = heading;
   contentInput.value = content;
   postElement.remove();
 };
+
+window.postdelete = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+
+deleteSelectedBtn.addEventListener('click', deleteSelectedPosts);
+const checkboxes = document.querySelectorAll('.post-checkbox');
+function deleteSelectedPosts() {
+  
+  checkboxes.forEach(checkbox => {
+    checkbox.parentElement.remove();
+  });
+  
+  
+}
